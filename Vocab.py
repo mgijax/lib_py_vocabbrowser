eed7cc0ff31d5458e927baed3fcc4371b1cec371
jpc <>
2000-09-26 20:37:56 +0000

    Modified get* methods to check for graph object, returning an empty list if the graph hasn't been defined.  Also added comments to all methods. --jpc

--- Vocab.py
+++ Vocab.py
@@ -7,26 +7,96 @@ class Vocab:
     """
     
     def __init__(self):
+        """
+        #  Requires:
+        #  Effects:
+        #    constructor
+        #  Modifies:
+        #    self.graph: DAG object
+        #    self.etypes: list (of edge types)
+        #  Returns:
+        #  Exceptions:
+        """
 
         self.graph = None
         self.etypes = []
 
     def findNode(self, id):
+        """
+        #  Requires:
+        #    id: string
+        #  Effects:
+        #    Abstract method to be overridden by subclasses
+        #  Modifies:
+        #  Returns:
+        #  Exceptions:
+        """
 
-        pass
+        return None
 
     def getParentsOf(self, node):
+        """
+        #  Requires:
+        #    node: Node object
+        #  Effects:
+        #    If self.graph exists, returns the node's parents; otherwise
+        #    returns an empty list
+        #  Modifies:
+        #  Returns:
+        #    list of tuples containing nodes and edge types
+        #  Exceptions:
+        """
 
-        return self.graph.getParentsOf(node)
+        if self.graph == None:
+            return []
+        else:
+            return self.graph.getParentsOf(node)
 
     def getChildrenOf(self, node):
+        """
+        #  Requires:
+        #    node: Node object
+        #  Effects:
+        #    If self.graph exists, returns the node's children; otherwise
+        #    returns an empty list
+        #  Modifies:
+        #  Returns:
+        #    list of tuples containing nodes and edge types
+        #  Exceptions:
+        """
 
-        return self.graph.getChildrenOf(node)
+        if self.graph == None:
+            return []
+        else:
+            return self.graph.getChildrenOf(node)
 
     def getPathsTo(self, node):
+        """
+        #  Requires:
+        #    node: Node object
+        #  Effects:
+        #    If self.graph exists, calls its methods to return all paths
+        #    to node; otherwise returns an empty list
+        #  Modifies:
+        #  Returns:
+        #    list of lists, each containing tuples of nodes and edge types
+        #  Exceptions:
+        """
 
-        return self.graph.getPathsTo(node)
+        if self.graph == None:
+            return []
+        else:
+            return self.graph.getPathsTo(node)
 
     def getEdgeTypes(self):
+        """
+        #  Requires:
+        #  Effects:
+        #    Returns a list of edge types defined for the vocabulary
+        #  Modifies:
+        #  Returns:
+        #    self.etypes
+        #  Exceptions:
+        """
 
         return self.etypes
